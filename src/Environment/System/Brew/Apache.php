a59bd832cd25a74620d144cba42a8ed3d2c6df7b
Jamie Sykes <jamie.sykes@creode.co.uk>
2019-10-25 12:21:24 +0100

    Adds dependency of Config to load in apache files.

--- src/Environment/System/Brew/Apache.php
+++ src/Environment/System/Brew/Apache.php
@@ -3,6 +3,8 @@
 namespace Cdev\Local\Environment\System\Brew;
 
 use Creode\System\Command;
+use Config;
+use Pear;
 
 class Apache extends Command {
     const COMMAND = 'apachectl';
@@ -14,6 +16,19 @@ class Apache extends Command {
      */
     private function initialise() {
         // TODO: Not sure here if to do something where I setup the 
+        $config = new Config();
+
+        $file = '/usr/local/etc/httpd/extra/httpd-vhosts.conf';
+        $root = $config->parseConfig($file, 'apache');
+
+        var_dump($root);
+
+        if (PEAR::isError($root)) {
+            echo 'Error reading config: ' . $root->getMessage() . "\n";
+            exit(1);
+        }
+
+        var_dump('it read it!');
     }
 
     /**
@@ -23,6 +38,7 @@ class Apache extends Command {
      * @return void
      */
     public function start($path) {
+        $this->initialise();
         $this->runExternalCommand('sudo ' . $this::COMMAND, ['-k', 'start'], $path);
     }
 
